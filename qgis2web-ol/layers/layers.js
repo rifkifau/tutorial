48fd1c8a673622982e6199fbf4c1514eb51e861d
Rifki Fauzi <versitani@gmail.com>
2019-10-08 09:24:08 +0700

    Update layers.js

--- qgis2web-ol/layers/layers.js
+++ qgis2web-ol/layers/layers.js
@@ -5,6 +5,8 @@ var baseLayer = new ol.layer.Tile({
     source: new ol.source.OSM()
 });
 var baseLayer2 = new ol.layer.Tile({
+      'title': 'MapBox',
+      'type': 'base',
       source: new ol.source.XYZ({
         url: 'https://api.mapbox.com/styles/v1/versitani/cjznhzxit03h51cofrb1lfw2f/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoidmVyc2l0YW5pIiwiYSI6ImNqeWUxM3BrODB5MDIzbXA4NzQzOW9idGMifQ.MUiosR2LCaSTV9q6ry4GWw',
         crossOrigin:"anonymous"
